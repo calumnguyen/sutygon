ee59be9c63dbd78d2b090a3f0ef9c4f0fa304a6f
Abdullah108 <abdullahpro44@gmail.com>
2021-02-04 05:23:38 +0500

    added build directory

---
+++ build/assets/js/fade.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  // $(".light-logo").hide();
+   
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 120) {
+        $(".light-logo").show();
+      } else {
+        $(".light-logo").hide();
+      }
+    
+    });
+
+
+
+
+
+    $("#fp-nav ul").attr("class","navdot");
+
+   $('.navdot li:eq(1)').click(function(){
+    $('.light-logo').show();
+    alert("hello");
+   });
+
+  })
+
