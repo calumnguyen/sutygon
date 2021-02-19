0ae6c1363f2998a5e7fa2c12950366577abd1afc
rameezamemon1 <rameeza.memon1@gmail.com>
2021-02-20 01:43:08 +0500

    base_url added

--- src/actions/customer.js
+++ src/actions/customer.js
@@ -16,7 +16,7 @@ import {
 
 const BASE_URL =
   process.env.NODE_ENV == "production"
-    ? process.env.BASE_URL
+    ? "https://sutygonbackend.herokuapp.com"
     : "http://localhost:5000";
 export const login = (username, password) => async (dispatch) => {
   dispatch({
